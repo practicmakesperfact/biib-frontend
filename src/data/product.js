026b990b03fcc9c3fcc2f88324762723057571ae
Haymanot Asmare <hasmare463@gmail.com>
2025-08-22 01:29:59 -0700

    add data in produc.js

---
+++ src/data/product.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    title: "Modern House Plan",
+    price: 120,
+    sqft: 2500,
+    beds: 3,
+    baths: 2,
+    floors: 2,
+    img: "/house1.jpg",
+  },
+  {
+    id: 2,
+    title: "Ranch Style Home",
+    price: 90,
+    sqft: 1800,
+    beds: 2,
+    baths: 2,
+    floors: 1,
+    img: "/house2.jpg",
+  },
+  {
+    id: 3,
+    title: "Luxury Villa",
+    price: 250,
+    sqft: 4000,
+    beds: 5,
+    baths: 4,
+    floors: 2,
+    img: "/house3.jpg",
+  },
+];
+
+export default products;
