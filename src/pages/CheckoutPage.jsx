095f5eb97d469f6d722f53b79e77a27a9a5212e1
Haymanot Asmare <hasmare463@gmail.com>
2025-08-22 01:50:06 -0700

    add CheckoutPage.jsx for order finalization flow

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,9 @@
+import Checkout from "../components/Checkout";
+
+export default function CheckoutPage() {
+  return (
+    <div className="max-w-6xl mx-auto px-6 py-10">
+      <Checkout />
+    </div>
+  );
+}
