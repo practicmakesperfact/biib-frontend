873616fcad3acc07e0e48637172d0255d0db17b7
Haymanot Asmare <hasmare463@gmail.com>
2025-08-24 05:46:44 -0700

    wrap in cartProvider and use layout for shared UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-// components
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./hooks/useCart";
+
+// Layout wrapper
+import Layout from "./components/layout/Layout";
+
+// Shared components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
+import ServicesPage from "./pages/ServicesPage";
+import TutorialsPage from "./pages/TutorialsPage";
+import AccountPage from "./pages/AccountPage";
 import Checkout from "./pages/Checkout";
-// pages
-import Home from "./pages/Home";
-import Catalog from "./pages/Catalog";
 import SingleProduct from "./pages/SingleProduct";
 import Cart from "./pages/Cart";
 
-import Account from "./pages/Account";
-// ... other pages
-
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/product/:id" element={<SingleProduct />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/account" element={<Account />} />
-         
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <CartProvider>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-1">
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/catalog" element={<CatalogPage />} />
+                <Route path="/services" element={<ServicesPage />} />
+                <Route path="/tutorials" element={<TutorialsPage />} />
+                <Route path="/account" element={<AccountPage />} />
+              </Route>
+              <Route path="/product/:id" element={<SingleProduct />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
+
+export default App;
