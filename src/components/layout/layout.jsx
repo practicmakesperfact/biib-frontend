0e99ffcac58bc46776ecf374b29a82cf48ffd522
Haymanot Asmare <hasmare463@gmail.com>
2025-08-24 06:07:58 -0700

    wrap main by cartProvider

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,3 +1,4 @@
+
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import Header from "./Header";
@@ -9,18 +10,11 @@ export default function Layout() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Header with cart toggle */}
       <Header onOpenCart={() => setIsCartOpen(true)} />
-
-      {/* Main content */}
       <main className="flex-1">
         <Outlet />
       </main>
-
-      {/* Footer */}
       <Footer />
-
-      {/* Cart Sidebar */}
       <CartSidebar isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
     </div>
   );
